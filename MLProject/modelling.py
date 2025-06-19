3ec376bd9c362aa0263ee8d6581d243fc7c1f9b9
IqbalHardiyanto <muhiqbal1059@gmail.com>
2025-06-19 21:51:15 +0700

    fix config

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -33,7 +33,7 @@ print(f"Target classes: {y.unique()}")
 X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42, stratify=y_temp)
 
-# set name experiment
+# Set name experiment
 mlflow.set_experiment("Soil_Classification_CI")
 
 print("Starting MLflow Tracking...")
