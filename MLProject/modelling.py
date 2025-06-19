b7cd5546e5df3b35f3d8114f1afed3b903c18661
IqbalHardiyanto <muhiqbal1059@gmail.com>
2025-06-19 18:27:22 +0700

    rev conda

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -35,7 +35,7 @@ print(f"Target classes: {y.unique()}")
 X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=42, stratify=y)
 X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42, stratify=y_temp)
 
-# Set MLflow experiment
+
 mlflow.set_experiment("Soil_Classification_CI")
 
 print("Starting MLflow Tracking...")
