098ba1be2552b8bb11885e36cc81c09cc1777cb9
IqbalHardiyanto <muhiqbal1059@gmail.com>
2025-06-19 23:11:25 +0700

    test docker

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -29,11 +29,11 @@ print(f"Features: {feature_columns}")
 print(f"Target: {target_column}")
 print(f"Target classes: {y.unique()}")
 
-# Split dataset
+# Split data
 X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42, stratify=y_temp)
 
-# Set nama experimen
+# Set nama eksperimen
 mlflow.set_experiment("Soil_Classification_CI")
 
 print("Starting MLflow Tracking...")
