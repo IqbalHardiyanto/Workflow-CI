db1a5fc46a7687da36c1aedbec9919272d2b4b70
IqbalHardiyanto <muhiqbal1059@gmail.com>
2025-06-19 20:33:22 +0700

    fix runID

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -31,8 +31,8 @@ print(f"Features: {feature_columns}")
 print(f"Target: {target_column}")
 print(f"Target classes: {y.unique()}")
 
-# Split data
-X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=42, stratify=y)
+# Split dataset
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42, stratify=y_temp)
 
 
